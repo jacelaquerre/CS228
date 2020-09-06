33ecf880ee881ba70295836cd7ae8e0c2418f6c0
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 16:08:08 -0400

    next steps done

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,6 +1,9 @@
 var controllerOptions = {};
 var i = 0;
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
 Leap.loop(controllerOptions, function(frame) {
     console.log(i)
+    circle(x, y, 50);
     }
 );
