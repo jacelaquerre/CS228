ed80131281773326fb615b237af4fda10c630c08
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 16:14:48 -0400

    random number for circle movement

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,8 +2,11 @@ var controllerOptions = {};
 var i = 0;
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
+var rand = 0;
 Leap.loop(controllerOptions, function(frame) {
     console.log(i)
-    circle(x, y, 50);
+    rand = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1)
+    circle(x + rand, y, 50);
+
     }
 );
