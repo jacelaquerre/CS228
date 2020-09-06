08330ff2ab6ea4b025885cf35e941ebfc2cee0c9
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 17:08:12 -0400

    next steps

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,11 +1,13 @@
 var controllerOptions = {};
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
-var rand = 0;
+var randx = 0;
+var randy = 0;
 Leap.loop(controllerOptions, function(frame) {
     clear();
-    rand = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
-    circle(x + rand, y, 50);
+    randx = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
+    randy = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
+    circle(x + randx, y + randy, 50);
     }
 
 );
