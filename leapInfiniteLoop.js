59da9580689e76c7ca989ca841e906a4d30e5431
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 16:56:52 -0400

    clear() and remove i

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,12 +1,11 @@
 var controllerOptions = {};
-var i = 0;
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 var rand = 0;
 Leap.loop(controllerOptions, function(frame) {
-    console.log(i)
-    rand = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1)
+    clear();
+    rand = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
     circle(x + rand, y, 50);
-
     }
+
 );
