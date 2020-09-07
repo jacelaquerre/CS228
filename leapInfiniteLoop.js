a5f8c03c4a91305c24dd39df1fa28ee81f11fe6e
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 22:16:39 -0400

    next steps

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,6 +2,10 @@ var controllerOptions = {};
 var x = 0;//window.innerWidth / 2;
 var y = 0;//window.innerHeight / 2;
 var z = 0;
+var rawXMin = 999999;
+var rawXMax = 1;
+var rawYMin = 999999;
+var rawYMax = 1;
 
 Leap.loop(controllerOptions, function(frame) {
     clear();
@@ -22,13 +26,13 @@ function HandleFrame(frame) {
 function HandleHand(hand) {
     var finger;
     for (var i = 0; i < hand.fingers.length; i++) {
-        // Check if the finger is an index finger
         finger = hand.fingers[i];
         HandleFinger(finger)
     }
 }
 
 function HandleFinger(finger) {
+    // Check if the finger is an index finger
     if (finger.type === 1) {
         x = finger.tipPosition[0];
         y = finger.tipPosition[1];
