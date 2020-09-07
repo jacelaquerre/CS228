b540d723434924c6a89267ea3e393ee1079237da
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-07 16:34:27 -0400

    next steps

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,16 +1,39 @@
 var controllerOptions = {};
-var x = 0;//window.innerWidth / 2;
-var y = 0;//window.innerHeight / 2;
+var x = 0;
+var y = 0;
 var z = 0;
-var rawXMin = 999999;
+var rawXMin = 9999;
 var rawXMax = 1;
-var rawYMin = 999999;
+var rawYMin = 999;
 var rawYMax = 1;
+var newX;
+var newY;
 
 Leap.loop(controllerOptions, function(frame) {
     clear();
     HandleFrame(frame)
-    circle(x, y, 50);
+    if (x < rawXMin) {
+        rawXMin = x;
+    }
+    if (x > rawXMax) {
+        rawXMin = x;
+    }
+    if (y < rawYMin) {
+        rawYMin = y;
+    }
+    if (y > rawYMax) {
+        rawYMax = y;
+    }
+    //console.log(rawXMin)
+    //console.log(rawXMin)
+    //console.log(rawYMin) // always 0
+
+    y = -y + (window.innerHeight)
+    console.log(y);
+    newX = (((x - rawXMin) * (window.innerWidth - 0)) / (rawXMax - rawXMin)) + 0
+    newY = (((y - rawYMin) * (window.innerHeight - 0)) / (rawYMax - rawYMin)) + 0
+    console.log(newY);
+    circle(newX, newY, 50);
 }
 );
 
@@ -36,6 +59,7 @@ function HandleFinger(finger) {
     if (finger.type === 1) {
         x = finger.tipPosition[0];
         y = finger.tipPosition[1];
+        //y = (-y)
         z = finger.tipPosition[2];
     }
 }
