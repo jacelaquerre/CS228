749d546426315e7dc0c702ca7e89db011ba7a06f
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-06 21:51:52 -0400

    next steps

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,13 +3,21 @@ var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 var randx = 0;
 var randy = 0;
+var Hand;
 Leap.loop(controllerOptions, function(frame) {
     //clear();
     //randx = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
     //randy = Math.floor(Math.random() * (1 - (-1) + 1) ) + (-1);
     //circle(x + randx, y + randy, 50);
-    if (frame.hands.length == 1) {
-        console.log(frame.hands);
+    if (frame.hands.length === 1) {
+        //console.log(frame.hands[0]);
+        Hand = frame.hands[0];
+        //console.log(Hand.fingers);
+        for (var i = 0; i < Hand.fingers.length; i++) {
+            if (Hand.fingers[i].type === 1) {
+                console.log(Hand.fingers[i].tipPosition);
+            }
+        }
     }
 }
 );
