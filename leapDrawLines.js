a3c5cbdfa35f694d47c9002eb1c9491813c339c7
Jace Laquerre <jace.laquerre@uvm.edu>
2020-09-13 16:34:39 -0400

    starting on deliverable 2

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -2,9 +2,9 @@ var controllerOptions = {};
 var x = 0;
 var y = 0;
 var z = 0;
-var rawXMin = 9999;
+var rawXMin = 1000;
 var rawXMax = 1;
-var rawYMin = 999;
+var rawYMin = 1000;
 var rawYMax = 1;
 var newX;
 var newY;
@@ -12,23 +12,11 @@ var newY;
 Leap.loop(controllerOptions, function(frame) {
         clear();
         HandleFrame(frame)
-        if (x < rawXMin) {
-            rawXMin = x;
-        }
-        if (x > rawXMax) {
-            rawXMin = x;
-        }
-        if (y < rawYMin) {
-            rawYMin = y;
-        }
-        if (y > rawYMax) {
-            rawYMax = y;
-        }
 
         y = -y + (window.innerHeight)
-        newX = (((x - rawXMin) * (window.innerWidth - 0)) / (rawXMax - rawXMin)) + 0
-        newY = (((y - rawYMin) * (window.innerHeight - 0)) / (rawYMax - rawYMin)) + 0
-        console.log(newY);
+        newX = (((x - rawXMin) * (window.innerWidth)) / (rawXMax - rawXMin))
+        newY = (((y - rawYMin) * (window.innerHeight)) / (rawYMax - rawYMin))
+
         circle(newX, newY, 50);
     }
 );
@@ -44,18 +32,30 @@ function HandleFrame(frame) {
 
 function HandleHand(hand) {
     var finger;
-    for (var i = 0; i < hand.fingers.length; i++) {
+    for (var i = 0; i < (hand.fingers).length; i++) {
         finger = hand.fingers[i];
         HandleFinger(finger)
     }
+    //for (var i = 0; i < (hand.fingers).length; i++) {
+    //    finger = hand.fingers[i];
+    //    HandleFinger(finger)
+    //}
 }
 
 function HandleFinger(finger) {
-    // Check if the finger is an index finger
-    if (finger.type === 1) {
-        x = finger.tipPosition[0];
-        y = finger.tipPosition[1];
-        //y = (-y)
-        z = finger.tipPosition[2];
+    x = finger.tipPosition[0];
+    y = finger.tipPosition[1];
+    z = finger.tipPosition[2];
+    if (x < rawXMin) {
+        rawXMin = x;
+    }
+    if (x > rawXMax) {
+        rawXMin = x;
+    }
+    if (y < rawYMin) {
+        rawYMin = y;
+    }
+    if (y > rawYMax) {
+        rawYMax = y;
     }
 }
